83393ff6f91608536cfe9d0de9579fc9e9adff90
atn95 <antannguyen2000@gmail.com>
2022-08-26 18:15:52 -0500

    Hamburger button done

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,47 +8,55 @@ export default function NavBar(): ReactElement {
 	const currentRoute = router.pathname;
 
 	return (
-		<div className={styles.container}>
-			<Link href={'/'}>
-				<div
-					className={[styles['nav-item'], currentRoute == '/' ? styles['active'] : undefined].join(
-						' '
-					)}>
-					<div>{'//1. <Home />'}</div>
-					<div className={styles['border-animation']}></div>
+		<div className={styles['main-container']}>
+			<div className={styles.container}>
+				<Link href={'/'}>
+					<div className={[styles['nav-item'], currentRoute == '/' ? styles['active'] : undefined].join(' ')}>
+						<div>{'//1. <Home />'}</div>
+						<div className={styles['border-animation']}></div>
+					</div>
+				</Link>
+				<Link href={'/projects'}>
+					<div className={[styles['nav-item'], currentRoute == '/projects' ? styles['active'] : undefined].join(' ')}>
+						<div>{'//2.  <Projects/>'}</div>
+						<div className={styles['border-animation']}></div>
+					</div>
+				</Link>
+				<Link href={'/about'}>
+					<div className={[styles['nav-item'], currentRoute == '/about' ? styles['active'] : undefined].join(' ')}>
+						<div>{'//3.  <About/>'}</div>
+						<div className={styles['border-animation']}></div>
+					</div>
+				</Link>
+				<Link href={'/contact'}>
+					<div className={[styles['nav-item'], currentRoute == '/contact' ? styles['active'] : undefined].join(' ')}>
+						<div>{'//4.  <Contact/>'}</div>
+						<div className={styles['border-animation']}></div>
+					</div>
+				</Link>
+			</div>
+			<div className={styles['mobile-nav-container']}>
+				<div className={styles['mobile-header']}>
+					<div className={styles['open-btn']}>
+						<p className={styles['open-btn-text']}>㊂</p>
+						<Link href={'/'}>
+							<div className={styles['nav-btn']}>{'//1. <Home />'}</div>
+						</Link>
+						<Link href={'/projects'}>
+							<div className={styles['nav-btn']}>{'//2. <Projects />'}</div>
+						</Link>
+						<Link href={'/contact'}>
+							<div className={styles['nav-btn']}>{'//3. <Contact />'}</div>
+						</Link>
+						<Link href={'/about'}>
+							<div className={styles['nav-btn']}>{'//4. <About />'}</div>
+						</Link>
+					</div>
+					<div className={styles['head-text-cont']}>
+						<h2 className={styles['head-text']}>An T. Nguyen</h2>
+					</div>
 				</div>
-			</Link>
-
-			<Link href={'/projects'}>
-				<div
-					className={[
-						styles['nav-item'],
-						currentRoute == '/projects' ? styles['active'] : undefined,
-					].join(' ')}>
-					<div>{'//2.  <Projects/>'}</div>
-					<div className={styles['border-animation']}></div>
-				</div>
-			</Link>
-			<Link href={'/about'}>
-				<div
-					className={[
-						styles['nav-item'],
-						currentRoute == '/about' ? styles['active'] : undefined,
-					].join(' ')}>
-					<div>{'//3.  <About/>'}</div>
-					<div className={styles['border-animation']}></div>
-				</div>
-			</Link>
-			<Link href={'/contact'}>
-				<div
-					className={[
-						styles['nav-item'],
-						currentRoute == '/contact' ? styles['active'] : undefined,
-					].join(' ')}>
-					<div>{'//4.  <Contact/>'}</div>
-					<div className={styles['border-animation']}></div>
-				</div>
-			</Link>
+			</div>
 		</div>
 	);
 }
